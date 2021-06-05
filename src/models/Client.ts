301daded49e74073e4b20a9987aa0b1e8b90c426
andersonprax <andersonintendope@gmail.com>
2021-06-05 15:17:55 -0300

    Criação da migration client

---
+++ src/models/Client.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, Long, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("clients")
+class Client {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    cpf: string;
+
+    @Column() // { select: false }
+    telefone: string;
+
+    @Column()
+    celular: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    tipo_sanguineo: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+}
+
+export default Client;
