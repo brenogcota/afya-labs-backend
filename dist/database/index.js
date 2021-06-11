5b43aca48200a7fcc572c4906d86e3d37e91f343
brenogcota <brenocota4@gmail.com>
2021-06-11 13:53:12 -0300

    deploy

--- dist/database/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var typeorm_1 = require("typeorm");
-var con = typeorm_1.createConnection();
-exports.default = con;
--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import PermissionController from './controllers/PermissionController';
 import RoleController from './controllers/RoleController';
 import ProductController from './controllers/ProductController';
 
-import { is } from './middlewares/permission';
+import { is } from './middleWares/permission';
 
 const router = Router();
 
