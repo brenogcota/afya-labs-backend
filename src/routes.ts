3937fc7168b84f6dfa0fc31f7b2ed15f82b57bef
esxadm <aldicleiry@hotmail.com>
2021-06-18 21:24:51 -0300

    routes updated

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import UserController from './controllers/UserController';
 import SessionController from './controllers/SessionController';
 import PermissionController from './controllers/PermissionController';
 import RoleController from './controllers/RoleController';
-import ProductController from './controllers/ProductController';
 
 import { is } from './middlewares/permission';
 //import ChartController from './controllers/ChartController';
@@ -17,13 +16,7 @@ router.post('/sessions', SessionController.create);
 router.post('/permissions', PermissionController.create);
 router.post('/roles', RoleController.create);
 
-router.post('/products', is(["ROLE_ADMIN"]), ProductController.create);
-router.get('/products', is(["ROLE_ADMIN", "ROLE_USER"]), ProductController.index);
-router.get('/products/:id', is(["ROLE_ADMIN", "ROLE_USER"]), ProductController.show);
 
-//router.post('/charts', ChartController.create);
-//router.get('/charts', ChartController.index);
-//router.post('/charts', ChartController.create);
 router.post('/specialists', SpecialistController.create);
 //router.get('/specialists', SpecialistController.create);
 router.post('/clients', ClientController.create);
