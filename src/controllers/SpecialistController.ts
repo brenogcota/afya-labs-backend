c838402165bff90521e37d4e0ba3c248a0d7faf6
esxadm <aldicleiry@hotmail.com>
2021-06-19 14:25:45 -0300

    SpecialistController updated

--- src/controllers/SpecialistController.ts
+++ src/controllers/SpecialistController.ts
@@ -2,22 +2,29 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 //import ProfessionRepository from '../repositories/ProfessionRepository';
 import SpecialistRepository from '../repositories/SpecialistRepository';
+import UserRepository from '../repositories/UserRepository';
 
 class SpecialistController {
     
     async create(request: Request, response: Response){
         const specialistRepository = getCustomRepository(SpecialistRepository);
+        const userRepository = getCustomRepository(UserRepository);
 
-        const { registro, name, telefone, celular, email } = request.body;
+        const { registro, name, telefone, celular, email, user } = request.body;
 
         const existSpecialist = await specialistRepository.findOne({name});
 
-
         const findByName = await specialistRepository.find({name});
+
+        const existUser = await userRepository.findOne(user);
 
 
         if(existSpecialist) {
             return response.status(400).json({ message: 'Specialist already exists!' })
+        }
+
+        if(!existUser) {
+            return response.status(404).json({ message: 'User does not exist!' })
         }
 
         
@@ -27,6 +34,7 @@ class SpecialistController {
             telefone,
             celular,
             email,
+            user: existUser
         });
 
         if(findByName) {
