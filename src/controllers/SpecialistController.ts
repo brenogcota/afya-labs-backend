e0afc0a28d7c22c1e5ceaee2f9d079c8e4f15fe2
esxadm <aldicleiry@hotmail.com>
2021-06-19 14:28:26 -0300

    SpecialistController updated

--- src/controllers/SpecialistController.ts
+++ src/controllers/SpecialistController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
-import ProfessionRepository from '../repositories/ProfessionRepository';
+//import ProfessionRepository from '../repositories/ProfessionRepository';
 import SpecialistRepository from '../repositories/SpecialistRepository';
 import UserRepository from '../repositories/UserRepository';
 
@@ -8,34 +8,34 @@ class SpecialistController {
     
     async create(request: Request, response: Response){
         const specialistRepository = getCustomRepository(SpecialistRepository);
-
         const userRepository = getCustomRepository(UserRepository);
-        
-        const { registro, name, telefone, celular, email, user } = request.body;
 
+        const { registro, name, telefone, celular, email, user } = request.body;
 
         const existSpecialist = await specialistRepository.findOne({name});
 
-
         const findByName = await specialistRepository.find({name});
+
+        const existUser = await userRepository.findOne(user);
 
 
         if(existSpecialist) {
             return response.status(400).json({ message: 'Specialist already exists!' })
         }
 
+        if(!existUser) {
+            return response.status(404).json({ message: 'User does not exist!' })
+        }
 
-        const existsUsers = await userRepository.findByIds(user);
-
+        
         const specialist = specialistRepository.create({
             registro,//encontrar um jeito de fazer a confirmação
             name,
             telefone,
             celular,
             email,
-            user: existsUsers
+            user: existUser
         });
-
 
         if(findByName) {
             return response.status(200).json(specialist)
